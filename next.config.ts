9606c24d46545257e06591d5f4d18d3390619d45
dorothyramokgadi3-ship-it <dorothyramokgadi3@gmail.com>
2026-04-30 16:31:08 +0200

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+     1	import type { NextConfig } from "next";
+     2	
+     3	const nextConfig: NextConfig = {
+     4	  /* config options here */
+     5	  devIndicators: false
+     6	};
+     7	
+     8	export default nextConfig;
